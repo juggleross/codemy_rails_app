0b981c36440ac002b831f0f4ed1443d8cf84adcf
Vladislav Hilko <vladislav_hilko@epam.com>
2018-09-20 15:58:11 +0300

    Finished lesson 8

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -4,10 +4,16 @@ class V1::SessionsController < ApplicationController
     if user&.valid_password?(params[:password])
       render json: user.as_json(only: [:email, :authentication_token]), status: :created
     else
-      head :unathorize
+      head :unauthorized
     end
   end
 
   def destroy
+    current_user&.authentication_token = nil
+    if current_user&.save
+      head(:ok)
+    else
+      head(:unauthorized)
+    end
   end
 end
